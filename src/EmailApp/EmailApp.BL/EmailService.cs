3bdfc5a410f6867d8711f43519c247dc2c3f6fc1
Egor-kl <57603709+Egor-kl@users.noreply.github.com>
2020-05-16 15:00:44 +0300

    feat: add EmailApp project

---
+++ src/EmailApp/EmailApp.BL/EmailService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace EmailApp.BL
+{
+    public class EmailService
+    {
+        /// <summary>
+        /// Отправка сообщения.
+        /// </summary>
+        public void SendEmail()
+        {
+            MailMessage message = new MailMessage();
+            message.From = new MailAddress(Constants.MESSAGE_SENDER_ADDRESS, Constants.DISPLAY_NAME);
+            Console.WriteLine("Кому вы хотите отправить сообщение? Введите email адрес: ");
+
+            var emailAdress = Console.ReadLine();
+            message.To.Add(emailAdress);
+
+            Console.WriteLine("Введите тему сообщения: ");
+            message.Subject = Console.ReadLine();
+
+            Console.WriteLine("Введите текст сообщения: ");
+            message.Body = Console.ReadLine();
+
+            using (var client = new System.Net.Mail.SmtpClient(Constants.GMAIL_HOST))
+            {
+                client.Credentials = new NetworkCredential(Constants.MESSAGE_SENDER_ADDRESS, Constants.EMAIL_PASSWORD);
+                client.Port = 587;
+                client.EnableSsl = true;
+
+                client.Send(message);
+            }
+        }
+    }
+}
---
+++ src/EmailApp/EmailApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using EmailApp.BL;
+
+namespace EmailApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var emailService = new EmailService();
+            emailService.SendEmail();
+        }
+    }
+}
